3784bac8af51472cb305dd25a187b2f1f7ecdb01
GayaneMalkhasyan <gayanemalkhasyan06@gmail.com>
2022-10-04 16:04:25 +0300

    test

---
+++ src/tnayin1/label.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./style.css";
+
+// 1
+// function HelloWorld(){
+//     return <h1>hello</h1>
+// }
+// export default HelloWorld;
+
+
+// 2
+// export default class HelloWorld extends React.Component{
+//     render () {
+//         return <h1>hello Gay</h1>
+//     }
+// }
+
+// 3
+// export default function HelloWorld(){
+//     return <h1>hello,</h1>
+// }
